9764b956973bac72902b4627eeda82eb4966634a
SIDDHANT DIXIT <siddhant.dixit23@gmail.com>
2021-10-24 13:15:45 +0530

    Added Files

---
+++ app.js
@@ -0,0 +1,21 @@
+const morgan = require('morgan');
+
+/**
+ * Node.js Login Boilerplate
+ * https://node-login.braitsch.io
+ * (c) 2013-2021 Stephen Braitsch
+ **/
+
+//	https://www.npmjs.com/package/@braitsch/express
+const express = require('@braitsch/express');
+
+const app = express();
+app.use(morgan("dev"));
+
+express.log('./logs');
+
+express.http(app);
+
+express.init(__dirname, app, 'node-login', true);
+
+express.start(app);
